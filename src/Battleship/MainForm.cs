8f43aa09e7e4e85ea7555a4858ebd778590fe176
Adrian Kuczynski <kuczynski.adrian@gmail.com>
2023-06-18 16:08:52 +0200

    Fixed MainForm

--- src/Battleship/MainForm.cs
+++ src/Battleship/MainForm.cs
@@ -7,16 +7,18 @@ namespace Battleship
             InitializeComponent();
         }
 
-        private void NewGameButton_Click(object sender, EventArgs e)
+
+
+        private void NewGameButton_Click_1(object sender, EventArgs e)
         {
             var gameBoard = new GameBoard();
 
-            // init board 
             for (var row = 0; row < 10; row++)
             {
                 for (var column = 0; row < 10; row++)
                 {
                     var boardCell = GameBoardPanel.GetControlFromPosition(column, row);
+
 
                 }
             }
